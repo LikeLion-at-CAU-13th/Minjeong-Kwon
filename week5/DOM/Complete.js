31924b8584ed8e1098f8bbeca204ab830f7d90bb
minddoi <minddoi493@gmail.com>
2025-04-07 22:34:37 +0900

    design: 버튼에 아이콘 이미지 적용

--- week5/DOM/Complete.js
+++ week5/DOM/Complete.js
@@ -6,8 +6,22 @@ class Complete {
         this.row = new Div('', 'row').node;
 
         this.innerText = new Div(complete, 'text-box');
-        this.restoreBtn = new Button('되돌리기', 'restore-btn');
-        this.delBtn = new Button('삭제', 'del-btn');
+
+        // 되돌리기 버튼 및 이미지 아이콘
+        this.restoreBtn = new Button('', 'restore-btn');
+        const restoreIcon = new Image();
+        restoreIcon.src = './assets/restore.png';
+        restoreIcon.style.width = '20px';
+        restoreIcon.style.height = '20px';
+        this.restoreBtn.node.appendChild(restoreIcon);
+
+        // 삭제 버튼 및 이미지 아이콘
+        this.delBtn = new Button('', 'del-btn');
+        const deleteIcon = new Image();
+        deleteIcon.src = './assets/delete.png';
+        deleteIcon.style.width = '20px';
+        deleteIcon.style.height = '20px';
+        this.delBtn.node.appendChild(deleteIcon);
     }
 
     //만들어진 요소를 한 줄로 합쳐서 this.row에 넣고 반환
--- week5/DOM/Todo.js
+++ week5/DOM/Todo.js
@@ -6,8 +6,22 @@ class Todo {
         this.row = new Div('', 'row').node;
 
         this.innerText = new Div(todoText, 'text-box');
-        this.completeBtn = new Button('완료', 'complete-btn');
-        this.delBtn = new Button('삭제', 'del-btn');
+
+        //삭제 버튼 이미지.. 원래 이렇게 복잡한 것인가 ... 
+        this.completeBtn = new Button('', 'complete-btn');
+        const completeIcon = new Image();
+        completeIcon.src = './assets/check.png';
+        completeIcon.style.width = '20px';
+        completeIcon.style.height = '20px';
+        this.completeBtn.node.appendChild(completeIcon);
+
+
+        this.delBtn = new Button('', 'del-btn');
+        const deleteIcon = new Image();
+        deleteIcon.src = './assets/delete.png';
+        deleteIcon.style.width = '20px';
+        deleteIcon.style.height = '20px';
+        this.delBtn.node.appendChild(deleteIcon);
     }
 
     //만들어진 요소를 한 줄로 합쳐서 this.row에 넣고 반환
