69f87f3619918d82598f452c40f81c90b6b7c9c4
minddoi <minddoi493@gmail.com>
2025-06-25 00:37:12 +0900

    feat: 정답 제출 및 결과 메시지 API 추가

--- week12/src/Api.js
+++ week12/src/Api.js
@@ -15,3 +15,38 @@ export async function getQuizList() {
         console.error('에러 발생:', err);
     }
 }
+
+export async function submitAnswer(answers) {
+    try {
+        const answersArray = 
+            Object.entries(answers).map(([id, answer]) => ({
+            id: Number(id),
+            answer,
+        }));
+    
+        const res = await fetch(`${BASE_URL}/api/answers`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ answers: answersArray })
+        });
+
+        const result = await res.json();
+        console.log('정답 응답 결과:', result); 
+        return result;
+    } catch (err) {
+        console.error('정답 확인 실패:', err);
+        return false;
+    }
+}
+
+export async function getResult(score) {
+  try {
+    const res = await fetch(`${BASE_URL}/api/result?score=${score}`);
+    const data = await res.json();
+    return data.message;
+  } catch (err) {
+    console.error('결과 메시지 불러오기 실패:', err);
+  }
+}
