7d575947275c9f38d0f4e301c1ab6cac09d4d254
minddoi <minddoi493@gmail.com>
2025-06-24 23:57:08 +0900

    design: 제출 버튼 추가

--- week12/src/pages/QuizList.jsx
+++ week12/src/pages/QuizList.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { getQuizList, checkAnswer } from '../Api.js';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
 
 
 const QuizList = () => {
     const [quizList, setQuizList] = useState([]); //퀴즈 리스트 불러올때
+    const [answers, setAnswers] = useState({}); // 선택한 답 저장
 
     useEffect(() => {
         async function fetchQuiz() {
@@ -16,8 +16,25 @@ const QuizList = () => {
         fetchQuiz();
     }, []);
 
+    const handleSelect = (quizId, selectedAnswer) => {
+        setAnswers(prev => ({
+            ...prev,
+            [quizId]: selectedAnswer
+        }))
+    }
+
+    const handleSubmit = async () => {
+        let correct = 0;
+        for (const quiz of quizList) {
+            const selectedAnswer = answers[quiz.id];
+            if (!selectedAnswer) continue;
+            const isCorrect = await checkAnswer(quiz.id, selectedAnswer);
+            if (isCorrect) correct++;
+        }
+    }
+
   return (
-    <div>
+    <Container>
         <h1>QuizList</h1>
         {quizList.map((quiz) => (
         <div key={quiz.id}>
@@ -29,6 +46,7 @@ const QuizList = () => {
                             type = "radio"
                             name={`quiz-${quiz.id}`} //하나 고를 수 있게
                             value={ans}
+                            onChange={() => handleSelect(quiz.id, ans)}
                              />
                         {ans}
                     </label>
@@ -37,27 +55,39 @@ const QuizList = () => {
         </div>
         ))}
 
-        <StyledLink to = "/result">
-            제출하기
-        </StyledLink>
-    </div>
+        <ButtonContainer>
+        <SubmitButton onClick={handleSubmit}>
+          제출하기
+        </SubmitButton>
+      </ButtonContainer>
+    </Container>
   )
 }
 
 export default QuizList
 
-const StyledLink = styled(Link)`
+const Container = styled.div`
+  padding: 20px;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 30px;
+`;
+
+const SubmitButton = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 100px;
+  width: 120px;
   height: 50px;
   font-size: 16px;
-  color: #4a4a4a;
   background-color: #b8edfb;
+  color: #4a4a4a;
+  border: none;
   border-radius: 20px;
   cursor: pointer;
-  text-decoration: none;
   font-weight: 500;
   box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.1);
 `;
