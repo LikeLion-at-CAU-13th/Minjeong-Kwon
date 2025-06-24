91bd6ded17b8f052d0de6415d62afe24c9fd6f33
minddoi <minddoi493@gmail.com>
2025-06-24 23:45:09 +0900

    design: 선택지 디자인 수정

--- week12/src/pages/QuizList.jsx
+++ week12/src/pages/QuizList.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import { getQuizList } from '../Api.js';
+import { getQuizList, checkAnswer } from '../Api.js';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 
 const QuizList = () => {
-    const [quizList, setQuizList] = useState([]);
+    const [quizList, setQuizList] = useState([]); //퀴즈 리스트 불러올때
 
     useEffect(() => {
         async function fetchQuiz() {
@@ -20,15 +22,42 @@ const QuizList = () => {
         {quizList.map((quiz) => (
         <div key={quiz.id}>
             <h4> Q. {quiz.question} </h4>
-            <ul>
+            <form>
                 {quiz.answers.map((ans, idx) => (
-                    <li key={idx}>{ans}</li>
+                    <label key={idx} style={{ display: 'block', marginBottom: '8px' }}>
+                        <input 
+                            type = "radio"
+                            name={`quiz-${quiz.id}`} //하나 고를 수 있게
+                            value={ans}
+                             />
+                        {ans}
+                    </label>
                 ))}
-            </ul>
+            </form>
         </div>
         ))}
+
+        <StyledLink to = "/result">
+            제출하기
+        </StyledLink>
     </div>
   )
 }
 
 export default QuizList
+
+const StyledLink = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100px;
+  height: 50px;
+  font-size: 16px;
+  color: #4a4a4a;
+  background-color: #b8edfb;
+  border-radius: 20px;
+  cursor: pointer;
+  text-decoration: none;
+  font-weight: 500;
+  box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.1);
+`;
