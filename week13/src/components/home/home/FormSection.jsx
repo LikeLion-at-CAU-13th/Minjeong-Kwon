828054ec7cb254c95ff627a71adea915cf6d6d70
minddoi <minddoi493@gmail.com>
2025-06-30 23:28:37 +0900

    feat: 모달창 추가

--- week13/src/components/home/home/FormSection.jsx
+++ week13/src/components/home/home/FormSection.jsx
@@ -1,29 +1,47 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import Form from './Form'
 import { Button, Wrapper } from '../common'
 import { ThemeColorContext } from '../../../context/context'
 import { useNavigate } from 'react-router-dom'
 import { useSetRecoilState } from 'recoil'
 import { isSumittedAtom } from '../../../recoil/atom'
+import FormModal from './FormModal'
 
 const FormSection = () => {
     const mode = useContext(ThemeColorContext);
     const navigate = useNavigate();
-    const setItSubmitted = useSetRecoilState(isSumittedAtom);
+    const setIsSubmitted = useSetRecoilState(isSumittedAtom);
 
     const handleBtn = () => {
-        setItSubmitted(true);
+        setIsSubmitted(true);
         navigate('/mypage')
     }
+
+    const [showModal, setShowModal] = useState(false);
+
+    const handleOpenModal = () => {
+      setShowModal(true);
+    }
+
+    const handleConfirm = () => {
+        setIsSubmitted(true);
+        navigate('/mypage');
+    };
+
+    const handleCancel = () => {
+        setShowModal(false);
+    };
 
   return (
     <Wrapper>
         <Form type='home' inputType='이름'/>
         <Form type='email' inputType='이메일'/>
         <Form type='date' inputType='날짜'/>
-        <Button mode={mode.button} onClick={handleBtn}>
+        <Button mode={mode.button} onClick={handleOpenModal}>
             제출
         </Button>
+
+        {showModal && <FormModal onConfirm={handleConfirm} onCancel={handleCancel} />}
     </Wrapper>
   )
 }
