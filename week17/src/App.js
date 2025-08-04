93ded3b5380393a7317a317242697ffdd15f498e
minddoi <minddoi493@gmail.com>
2025-08-04 22:34:49 +0900

    week18 세션 내용

---
+++ week17/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Cart from './components/Cart';
+import ProductList from './components/ProductList';
+
+function App() {
+  return (
+    <div>
+      <Cart/>
+      <ProductList/>
+    </div>
+  );
+}
+
+export default App;
